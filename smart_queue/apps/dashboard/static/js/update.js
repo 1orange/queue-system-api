bb7c6119ec9fbfb59ffc8974b9928ba571636c1f
1orange <orangel33t@gmail.com>
2022-05-09 13:16:14 +0200

    tmp: refresh iteration

--- smart_queue/apps/dashboard/static/js/update.js
+++ smart_queue/apps/dashboard/static/js/update.js
@@ -2,10 +2,27 @@
 setInterval( function() {
     console.log("Queue fetch");
 
-    $('div#queue-tmp').load(location.href + " #queue-content");
-    $('div#queue-tmp').removeClass("hidden");
-    $('div#queue-content').remove();
-    $('div#queue-tmp').attr("id", "queue-content");
+    // var contnet = $('div#queue-tmp').load(location.href + " #queue-content");
+    // $('div#queue-tmp').removeClass("hidden");
+    // $('div#queue-content').remove();
+    // $('div#queue-tmp').attr("id", "queue-content");
+
+    var tmp;
+    $.ajax({
+        url:location.href,
+        type:'GET',
+        success: function(data) {
+            tmp = $('<div id="queue-tmp" class="hidden">').append(data).find('#queue-content');
+        }
+    });
+
+    if (!(tmp === undefined || tmp === null)) {
+        $('div#queue-tmp').load(tmp);
+        $('div#queue-tmp').removeClass("hidden");
+
+        $('div#queue-content').remove();
+        $('div#queue-tmp').attr("id", "queue-content");
+    }
 
 }, 30000); // each 30 sec
 
