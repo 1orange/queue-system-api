f5eb122984efde3b433099f9a9901fc982d58ffc
1orange <orangel33t@gmail.com>
2022-05-09 10:46:07 +0200

    tmp: jquery test

--- smart_queue/apps/dashboard/static/js/update.js
+++ smart_queue/apps/dashboard/static/js/update.js
@@ -2,19 +2,10 @@
 setInterval( function() {
     if(document.getElementById("no-queue-content") !== null)
     {
-        $.ajax({
-            url:location.href,
-            type:'GET',
-            success: function(data) {
-                var content = $('<div>').append(data).find('#queue-content');
-                $('div#queue-tmp').html( content );
+        $('div#queue-tmp').load(location.href + " #queue-content");
 
-            }
-        });
-
-        // $('div#no-queue-content').empty();
-        // $('div#no-queue-content').load(location.href + " #queue-content");
-        // $('div#no-queue-content').attr("id", "queue-content")    
+        $('div#queue-content').remove();
+        $('div#queue-tmp').attr("id", "queue-content");
     }
 }, 30000); // each 30 sec
 
