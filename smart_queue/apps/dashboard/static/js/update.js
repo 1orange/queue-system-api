1b1e9fbc9ae929fcb1fd26d1d2a678c476b8dc55
1orange <orangel33t@gmail.com>
2022-05-09 10:37:02 +0200

    tmp: jquery test

--- smart_queue/apps/dashboard/static/js/update.js
+++ smart_queue/apps/dashboard/static/js/update.js
@@ -2,16 +2,26 @@
 setInterval( function() {
     if(document.getElementById("no-queue-content") !== null)
     {
-        $('div#no-queue-content').empty();
-        $('div#no-queue-content').load(location.href + " #queue-content");
-        $('div#no-queue-content').attr("id", "queue-content")    
+        $.ajax({
+            url:location.href,
+            type:'GET',
+            success: function(data) {
+                var content = $('<div>').append(data).find('#queue-content');
+                $('div#queue-tmp').html( content );
+
+            }
+        });
+
+        // $('div#no-queue-content').empty();
+        // $('div#no-queue-content').load(location.href + " #queue-content");
+        // $('div#no-queue-content').attr("id", "queue-content")    
     }
 }, 30000); // each 30 sec
 
 // Check patient
 setInterval( function() {
     if(document.getElementById("no-patient-content") !== null)
-    {
+    {   
         $('div#no-patient-content').empty();
         $('div#no-patient-content').load(location.href + " #patient-content");
         $('div#no-patient-content').attr("id", "patient-content")    
