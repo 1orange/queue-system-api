d105908fdfa1ab43577201abaa8a08d3c5a2fb24
1orange <orangel33t@gmail.com>
2022-05-09 09:30:43 +0200

    feat: fix ajax load problem

--- smart_queue/apps/dashboard/static/js/update.js
+++ smart_queue/apps/dashboard/static/js/update.js
@@ -1,10 +1,12 @@
 // Check queue
 setInterval( function() {
+    $('div#queue').empty();
     $('div#queue').load(location.href + " #queue");
 }, 30000); // each 30 sec
 
 // Check patient
 setInterval( function() {
+    $('div#patient').empty();
     $('div#patient').load(location.href + " #patient");
 }, 30000); // each 30 sec
 
