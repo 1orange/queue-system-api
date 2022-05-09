c20899b8c084b05ecc1394040fca5b1b8ab9e58a
1orange <orangel33t@gmail.com>
2022-05-09 09:00:15 +0200

    feat: set interval to refresh

--- smart_queue/apps/dashboard/static/js/update.js
+++ smart_queue/apps/dashboard/static/js/update.js
@@ -1,3 +1,11 @@
-// Load queue
-$('div#queue').load('http://localhost:5000 #queue');
-$('div#patient').load('http://localhost:5000 #patient');
+// Check queue
+setInterval( function() {
+    $('div#queue').load('http://localhost:5000 #queue');
+}, 30000); // each 30 sec
+
+// Check patient
+setInterval( function() {
+    $('div#patient').load('http://localhost:5000 #patient');
+}, 30000); // each 30 sec
+
+
