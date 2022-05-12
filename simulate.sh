6382b0c7c5539e524ed10411d194c67b4970322d
Jean Dossa Tsimbazafy <493354@muni.cz>
2022-05-12 03:13:48 +0200

    feat: simulator script

---
+++ simulate.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+iterations=(100 1000 10000 100000)
+for iter in ${iterations[@]}; do
+    echo "Simulating ${iter} iterations:"
+    python run simulator.py --iterations $iter >> /dev/null 2>&1 &
+    PID=$!
+
+    echo "Waiting to end of simulation"
+    wait $PID
+
+    echo "End of simulation"
+done
